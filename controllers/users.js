0585ab3ac5700688679f88f72b920527aa981443
Elena Vasilenko <elenawest92@yandex.ru>
2023-10-30 13:34:35 +0300

    отладка сервера

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-const { SECRET_KEY, NODE_ENV } = process.env;
+const { JWT_SECRET, NODE_ENV } = process.env;
 
 const BadRequestError = require('../errors/BadRequestError');
 const NotFoundError = require('../errors/NotFoundError');
@@ -36,7 +36,7 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? SECRET_KEY : 'hello', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'hello', { expiresIn: '7d' });
       res.send({ token });
     })
     .catch(next);
